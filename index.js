6eac022bda272bb9fd7eef6bbdb5f0020ec0b517
RobertaCasu <robertacasu@ymail.com>
2018-11-28 11:29:08 +0100

    collegamenti vari

--- index.js
+++ index.js
@@ -2,7 +2,21 @@ var express = require('express');
 var app = express();
 
 var bodyParser = require('body-parser');
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+
+var path = require('path');
+
+// middleware per file statici
+app.use('/js', express.static (path.join(__dirname, 'public', 'js' )));
+app.use('/immagini', express.static (path.join(__dirname, 'public', 'images' )));
+
+app.use('/bootstrap', express.static (path.join(__dirname, 'node_modules', 'bootstrap', 'dist' )));
+app.use('/jquery', express.static (path.join(__dirname, 'node_modules', 'jquery', 'dist' )));
+app.use('/popper', express.static (path.join(__dirname, 'node_modules', 'popper.js', 'dist', 'umd' )));
+
+
+
+
 
 //ROUTING DI BASE -> req rest li scelgo io
 app.get('/', function (request, response) {
